50fd5ca99d5c5a94622570f6701a7706bbe7b60a
ChokunPlayZ <chokun.swc555@gmail.com>
2021-10-23 01:15:09 +0700

    fix index broken

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const axios = require('axios');
 const thaipost = require('./lib/thaipost.js');
 
-var thaiposttoken = 'sssZ1QwC5NIRIW?S7WRMGIDPWXcL#S*JJOUAVAtCHR2BMH9WRJnK*SRQ8AOB;ZvFGVsQ$M#WsFEOwP=Z2PCCYK+PxO+CSAVIIHEMkKV'
+var thaiposttoken = 'Token Here'
 var trackingcode = 'Tracking Nimber Here'
 
 thaipost.getPackage(thaiposttoken, trackingcode).then((package) => {
